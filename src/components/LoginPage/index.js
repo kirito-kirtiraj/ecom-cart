36dbb514ec92b436122d31fa94a060a7b4b960d9
Kirtiraj <kirtirajscoe@gmail.com>
2021-05-08 22:12:58 -0600

    google auth added

---
+++ src/components/LoginPage/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import LoginGoogle from "./Login";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <LoginGoogle />
+    </div>
+  );
+};
+
+export default LoginPage;
