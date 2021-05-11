3d60b65208767c8faf5e9601ade36029072cc3cf
Kirtiraj <kirtirajscoe@gmail.com>
2021-05-11 01:37:07 -0600

    Cart complete

--- src/stateManagement/actions.js
+++ src/stateManagement/actions.js
@@ -7,3 +7,8 @@ export const ADD_TO_CART = "ADD_TO_CART";
 export const addToCart = (payload) => {
   return { type: ADD_TO_CART, payload };
 };
+
+export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
+export const removeFromCart = (payload) => {
+  return { type: REMOVE_FROM_CART, payload };
+};
--- src/stateManagement/reducers.js
+++ src/stateManagement/reducers.js
@@ -1,4 +1,4 @@
-import { SET_LOG_IN, ADD_TO_CART } from "./actions";
+import { SET_LOG_IN, ADD_TO_CART, REMOVE_FROM_CART } from "./actions";
 
 const initialState = {
   isLoggedIn: false,
@@ -11,10 +11,17 @@ const rootReducer = (state = initialState, action) => {
       return Object.assign({}, state, { isLoggedIn: action.payload });
 
     case ADD_TO_CART:
-      console.log(state.cart.concat(action.payload))
+      console.log(state.cart.concat(action.payload));
       return Object.assign({}, state, {
         cart: state.cart.concat(action.payload),
       });
+
+    case REMOVE_FROM_CART:
+      const cart = state.cart.filter(
+        (product) => product.id !== action.payload
+      );
+      console.log(cart);
+      return Object.assign({}, state, { cart });
 
     default:
       return state;
